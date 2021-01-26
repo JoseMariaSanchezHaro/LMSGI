72e5a40283eddb39dbb5d5a71f248c2628722d79
Jose Maria Sanchez Haro <jomasanha@gmail.com>
2021-01-26 12:46:11 +0100

    commit 26/01/2021 trabajo de clase

--- HTML Definitivo/HTML/JQuery/CaronteSistemas/js/app.js
+++ HTML Definitivo/HTML/JQuery/CaronteSistemas/js/app.js
@@ -1,16 +1,26 @@
 let doc = $(document);
+/* al cargar la pagina completa en RAM del ordenador/movil se ejecuta la funcion iniciar() */
 doc.ready(iniciar);
 
 
 function iniciar(){
-    console.log ('iniciando');
-    let opcion = $("header #header-content nav#principal a");
-    $(opcion).on('click', cargar);
-    $("header #navicon").on('click', mostrarmenu);
+    console.log ('documento iniciando');/* debug */
+    $("main").load("html/home.html");
+    let opcion = $("header #header-content nav a").on('click', mostrarMenu);
+    let home = $("header #home").on('click', mostrarMenu);
+    let navicon = $("header #navicon").on('click', mostrarNavPrincipal);
 }
+function mostrarNavPrincipal(){
+    console.log('navicon')
+    $("header #header-content nav").slideToggle();
+}
+function mostrarMenu(){
 
-function mostrarmenu(){
-    $("header #principal").slideToggle();
+    $(this).addClass("active")
+    let id = $(this).attr('id');
+    let ruta = "html/"+id+".html";
+    $("main").load(ruta);
+
 
 }
 
